d2a5086128f41dc372d5c8c32ea9a71f5fcfa112
Cristian Thompson <Thompsonacristian@gmail.com>
2017-06-01 12:11:09 -0300

    Change how time is calculated and presented in HTML to be accurate whether window is focus or not by using subtraction versus active addition

--- static/timelogic.js
+++ static/timelogic.js
@@ -28,16 +28,14 @@ $.ajaxSetup({
 const secondsToAppend = document.getElementById('second');
 const minutestoAppend = document.getElementById('minute');
 const hourstoAppend = document.getElementById('hour');
-const controlstoAppend = document.getElementById('controls');
-const blockShifter = document.getElementsByClassName('randBlock');
 const workedList = document.getElementById('worked');
 const popup = document.getElementById('popup');
 const body = document.getElementsByClassName('main-body')[0];
 let title = document.getElementById('title');
 let actualLoop;
 let currentWork = [];
-let timeStart, timeEnd, verifiedTime, workedTime, startDate, endDate;
-let loopInterval, minutes, hours, afterNoon;
+let timeStart, timeEnd, verifiedTime, startDate, endDate;
+let minutes, hours, afterNoon;
 let totalHours = 0;
 let totalMinutes = 0;
 let totalSeconds = 0;
@@ -49,7 +47,7 @@ let formatTime = function(hoursToVerify, minutesToVerify) {
     hours = hoursToVerify;
   } else {
     hours = parseInt(hoursToVerify) - 12;
-    if (hours == 0) {
+    if (hours === 0) {
       hours = 12;
     }
   }
@@ -60,7 +58,7 @@ let formatTime = function(hoursToVerify, minutesToVerify) {
   }
   verifiedTime = hours + ":" + minutes + " " + afterNoon;
   return verifiedTime;
-}
+};
 
 let updateTotalTime = function(){
   if (totalSeconds >= 60) {
@@ -71,8 +69,8 @@ let updateTotalTime = function(){
     totalMinutes -= 60;
     totalHours++;
   }
-  document.getElementById('workedTotal').innerHTML = totalHours + " - Hours : " + totalMinutes + " - Minutes : " + totalSeconds.toFixed(2) + " - Seconds";
-}
+  document.getElementById('workedTotal').innerHTML = totalHours + " - Hours : " + totalMinutes + " - Minutes : " + totalSeconds + " - Seconds";
+};
 
 let formatDate = function() {
   let day = new Date().getDate();
@@ -80,7 +78,7 @@ let formatDate = function() {
   let year = new Date().getFullYear();
   let todayDate = month + '/' + day + '/' + year;
   return todayDate;
-}
+};
 
 let showPopup = function(buttonText, titleText) {
   document.getElementById('popup-prompt').innerHTML = titleText;
@@ -88,54 +86,68 @@ let showPopup = function(buttonText, titleText) {
   document.getElementsByClassName('popup-button')[0].innerHTML = buttonText;
   popup.classList.remove('popup-hidden');
   body.classList.add('body-hidden');
-}
+};
 
 let hidePopup = function() {
   popup.classList.add('popup-hidden');
   body.classList.remove('body-hidden');
   document.getElementsByClassName('popup-button')[0].id = "";
   document.getElementById('popup-prompt').innerHTML = "";
-}
+};
 
 let clearLog = function() {
   workedList.innerHTML = null;
   updateTotalTime();
-}
+};
+
+let trackTime = function(initialTime) {
+  let currentMinutes = 0;
+  let currentSeconds = 0;
+  let currentHours = 0;
+  let currentTimes = (Date.now() - initialTime) / 1000;
+  currentSeconds = Math.floor(currentTimes);
+  while (currentSeconds > 60) {
+    if (currentMinutes < 59) {
+      currentMinutes++;
+    } else {
+      currentMinutes = 0;
+      currentHours++;
+    }
+    currentSeconds -= 60;
+  }
+  if ($('#second').text() != currentSeconds.toString()) {
+    secondsToAppend.innerHTML = currentSeconds;
+  }
+  if ($('#minute').text() != currentMinutes.toString() && currentMinutes !== 0) {
+    minutestoAppend.innerHTML = currentMinutes;
+  } else if (currentMinutes === 0) {
+    if (minutestoAppend.innerHTML != "None Yet!"){
+      minutestoAppend.innerHTML = "None Yet!";
+    }
+  }
+  if ($('#hour').text() != currentHours.toString() && currentHours !== 0) {
+    hourstoAppend.innerHTML = currentHours;
+  } else if (currentHours === 0) {
+    if (hourstoAppend.innerHTML != "None Yet!") {
+      hourstoAppend.innerHTML = "None Yet!";
+    }
+  }
+  currentWork.splice(0, 1, currentSeconds);
+  currentWork.splice(1, 1, currentMinutes);
+  currentWork.splice(2, 1, currentHours);
+};
 
 let createLoop = function() {
-  const timeStarted = Date.now();
-  let loopStart = Date.now();
-  let seconds = 0;
-  let minutes = 0;
-  let hours = 0;
+  let timeStarted = Date.now();
   actualLoop = setInterval(function(){
-    const loopInterval = (Date.now() - loopStart) / 1000;
-    if (loopInterval <= 60){
-      secondsToAppend.innerHTML = loopInterval + 's';
-      currentWork.splice(0, 1, loopInterval);
-    } else {
-      if (minutes < 59){
-         minutes++;
-        minutestoAppend.innerHTML = minutes;
-        currentWork.splice(1, 1, minutes);
-      } else {
-        minutes = 0;
-        hours++;
-        currentWork.splice(1, 1, minutes);
-        currentWork.splice(2, 1, hours);
-        minutestoAppend.innerHTML = minutes;
-        hourstoAppend.innerHTML = hours;
-      }
-      loopStart = Date.now();
-      lastBlock = Math.floor((Math.random() * 9) + 5);
-    }
+    trackTime(timeStarted);
   }, 10);
-}
+};
 
 document.getElementById('start-prompt').onclick = function() {
    if (!actualLoop){
      let titleText;
-     if (title.value != "") {
+     if (title.value !== "") {
        titleText = "<h3>" + title.value.toString() + "</h3> is the correct title for this project?";
      } else {
        titleText = "<h3>Default</h3> is the correct title for this project?";
@@ -153,36 +165,48 @@ document.getElementById('start-prompt').onclick = function() {
          timeStart = verifiedTime;
          minutestoAppend.innerHTML = "None Yet!";
          hourstoAppend.innerHTML = "None Yet!";
-         startDate = formatDate()
+         startDate = formatDate();
        } else {
          return;
        }
-     }
+     };
   } else {
     return;
   }
-}
+};
 
 document.getElementById('Cancel').onclick = function() {
-  hidePopup()
-}
+  hidePopup();
+};
 
 document.getElementById('stop').onclick = function() {
   if (actualLoop) {
     clearInterval(actualLoop);
     actualLoop = null;
-    let loggedHours;
-    let loggedMinutes;
-    let loggedSeconds;
+    let loggedHours, loggedMinutes;
+    let loggedSeconds, projectTitle;
+    let saveText = null;
+    let saveButton = 'Save';
     let hours = new Date().getHours();
     let minutes = new Date().getMinutes();
     formatTime(hours, minutes);
     timeEnd = verifiedTime;
     endDate = formatDate();
-    if (title.value != ""){
+    if (title.value !== ""){
       projectTitle = title.value;
     } else {
       projectTitle = "Default";
+    }
+    totalSeconds += currentWork[0];
+    loggedSeconds = currentWork[0];
+    if (currentWork[1]) {
+      totalMinutes += currentWork[1];
+      loggedMinutes = currentWork[1];
+      saveText = '<p>Do you want to record the project: ' +
+        '<h3 style="display: inline; margin: 0;">' + projectTitle + '</h3></p><p>'+
+        'Started: ' + startDate + ' | ' + timeStart + '</p><p> -- ' + loggedMinutes +
+        ' minutes/' + loggedSeconds + ' seconds -- </p><p>Ended: ' + endDate +
+        ' | ' + timeEnd + '</p>';
     }
     if (currentWork[2]) {
       totalHours += currentWork[2];
@@ -193,22 +217,12 @@ document.getElementById('stop').onclick = function() {
         ' hours/ ' + loggedMinutes + ' minutes/' + loggedSeconds +
         ' seconds -- </p><p>Ended: ' + endDate + ' | ' + timeEnd + '</p>';
     }
-    if (currentWork[1]) {
-      totalMinutes += currentWork[1];
-      loggedMinutes = currentWork[1];
+    if (!saveText){
       saveText = '<p>Do you want to record the project: ' +
         '<h3 style="display: inline; margin: 0;">' + projectTitle + '</h3></p><p>'+
-        'Started: ' + startDate + ' | ' + timeStart + '</p><p> -- ' + loggedMinutes +
-        ' minutes/' + loggedSeconds + ' seconds -- </p><p>Ended: ' + endDate +
-        ' | ' + timeEnd + '</p>';
+        'Started: ' + startDate + ' | ' + timeStart + '</p><p> -- ' + loggedSeconds +
+        ' seconds -- </p><p>Ended: ' + endDate + ' | ' + timeEnd + '</p>';
     }
-    totalSeconds += currentWork[0];
-    loggedSeconds = currentWork[0];
-    saveText = '<p>Do you want to record the project: ' +
-      '<h3 style="display: inline; margin: 0;">' + projectTitle + '</h3></p><p>'+
-      'Started: ' + startDate + ' | ' + timeStart + '</p><p> -- ' + loggedSeconds +
-      ' seconds -- </p><p>Ended: ' + endDate + ' | ' + timeEnd + '</p>';
-    saveButton = 'Save';
     showPopup(saveButton, saveText);
     document.getElementById('Save').onclick = function() {
       hidePopup();
@@ -238,16 +252,16 @@ document.getElementById('stop').onclick = function() {
         }
       });
       updateTotalTime();
-    }
+    };
     title.disabled = false;
     currentWork = [];
   } else {
     return;
   }
-}
+};
 
 document.getElementById('clear-prompt').onclick = function() {
-  let titleText = "Are you sure you want to clear the visible log?"
+  let titleText = "Are you sure you want to clear the visible log?";
   let buttonText = "Clear";
   if (!actualLoop) {
     showPopup(buttonText, titleText);
@@ -258,16 +272,18 @@ document.getElementById('clear-prompt').onclick = function() {
       totalHours = 0;
       clearLog();
       hidePopup();
-    }
+    };
   }
-}
+};
 
 
 $('#project-title').on('keyup keypress', function(event) {
   var enterKey = event.keyCode || event.which;
   if (enterKey === 13) {
     event.preventDefault();
-    showStartPopup();
+    if (!actualLoop) {
+      document.getElementById('start-prompt').click();
+    }
     return false;
   }
-})
+});
